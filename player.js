efe87d95d12668f1f4e835123d2a5a16a19363c0
glebfadey <79185410+glebfadey@users.noreply.github.com>
2026-05-14 22:45:25 +0300

    Delete player.js

--- player.js
+++
@@ -1,9 +0,0 @@
-window.ScenarioPlayerSolid = class {
-    constructor() {
-        console.log("Scenario Player loaded");
-        this.render();
-    }
-    render() {
-        document.body.innerHTML += '<p style="color:white;">System Ready</p>';
-    }
-};
